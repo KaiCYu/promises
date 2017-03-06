556a9e4e1c3d31c2c6198497101c9f79855baa1d
KaiCYu <kaichyu@gmail.com>
2017-03-05 21:40:28 -0800

    finished callback review and promise constructor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,28 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    if (err) {
+      console.log('fs.readFile failed: ' + err);
+      callback(err);
+    } else {
+      var firstLine = content.split('\r')[0];
+      console.log(firstLine);
+      callback(err, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function (err, res) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
